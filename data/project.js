214ef1b2f3f61fa381d2e45330b39364107d9634
Aryan0169 <aryanagarwal170@gmail.com>
2023-07-22 20:25:14 +0530

    cards final scroll left

--- data/project.js
+++ data/project.js
@@ -7,11 +7,11 @@ const projectsData = [
         buttontitle: "Website",
     },
     {
-        title: 'PLINTH 2023',
-        description: 'Worked as a UI/UX Developer in making the website for the Annual techfest of LNMIIT, Jaipur.',
-        imageUrl: '../images/plinth.png',
-        link: 'https://www.plinth.co.in/',
+        title: 'Personal Portfolio',
+        description: 'This is my personal portfolio. I have developed as well as designed this on my own',
+        imageUrl: '../images/pfl.png',
+        link: 'https://www.aryanbansal02.netlify.app/',
         buttontitle: "Website",
-    },
+    }
     // Add more project objects as needed
 ];
