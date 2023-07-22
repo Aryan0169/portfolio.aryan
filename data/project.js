63ef2166af8bd27ebd1b11bd00b4dc9673b4be86
Aryan0169 <aryanagarwal170@gmail.com>
2023-07-22 20:01:43 +0530

    projectsnew

---
+++ data/project.js
@@ -0,0 +1,17 @@
+const projectsData = [
+    {
+        title: 'PLINTH 2023',
+        description: 'Worked as a UI/UX Developer in making the website for the Annual techfest of LNMIIT, Jaipur.',
+        imageUrl: '../images/plinth.png',
+        link: 'https://www.plinth.co.in/',
+        buttontitle: "Website",
+    },
+    {
+        title: 'PLINTH 2023',
+        description: 'Worked as a UI/UX Developer in making the website for the Annual techfest of LNMIIT, Jaipur.',
+        imageUrl: '../images/plinth.png',
+        link: 'https://www.plinth.co.in/',
+        buttontitle: "Website",
+    },
+    // Add more project objects as needed
+];
