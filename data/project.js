feca473587108116464a45595e1d74125800ac75
Aryan0169 <aryanagarwal170@gmail.com>
2023-07-23 21:38:40 +0530

    add file

---
+++ data/project.js
@@ -0,0 +1,31 @@
+const projectsData = [
+    {
+        title: 'PLINTH 2023',
+        description: 'Worked as a UI/UX Developer in making the website for the Annual techfest of LNMIIT, Jaipur.',
+        imageUrl: '../images/plinth.png',
+        link: 'https://www.plinth.co.in/',
+        buttontitle: "Website",
+    },
+    {
+        title: 'Personal Portfolio',
+        description: 'This is my personal portfolio. I have developed as well as designed this on my own',
+        imageUrl: '../images/pfl.png',
+        link: 'https://www.aryanbansal02.netlify.app/',
+        buttontitle: "Website",
+    },
+    {
+        title: 'Gesture Controlled BOT',
+        description: 'As a Design Lab project in III Sem, I created a bot that move according to the direction of the Hand',
+        imageUrl: '../images/plinth.png',
+        link: 'https://github.com/Aryan0169/Gesture-Control-Bot.git',
+        buttontitle: "GITHUB",
+    },
+    {
+        title: 'Design Projects',
+        description: 'This is my Behance Profile which contain all my design projects',
+        imageUrl: '../images/behance.png',
+        link: 'https://www.behance.net/aryanbansal6',
+        buttontitle: "Behance",
+    }
+    // Add more project objects as needed
+];
