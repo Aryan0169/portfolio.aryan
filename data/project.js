60ef63452d8597e69c54f64c204a9bcf67661fe4
Aryan0169 <aryanagarwal170@gmail.com>
2023-07-24 23:01:06 +0530

    added projects

--- data/project.js
+++ data/project.js
@@ -26,6 +26,20 @@ const projectsData = [
         imageUrl: '../images/behance.png',
         link: 'https://www.behance.net/aryanbansal6',
         buttontitle: "Behance",
+    },
+    {
+        title: 'UAV assisted multiser DF relayed system',
+        description: 'As a LUSIP summer intern I made use of NOMA technology combined with relay mechanism to make 6G+ communication better',
+        imageUrl: '../images/wireless.jpg',
+        link: 'coming.html',
+        buttontitle: "Certificate",
+    },
+    {
+        title: 'GDSC Website',
+        description: 'As a creative domain lead at GDSC I designed the website for our club',
+        imageUrl: '../images/gdsc.png',
+        link: 'https://www.figma.com/proto/PFQnOfa19onhSRTomS1Rz7/Website-GDSC?type=design&node-id=612-23658&t=AVq4nIfDq2iAXkXP-0&scaling=scale-down-width&page-id=0%3A1&starting-point-node-id=640%3A2515',
+        buttontitle: "Figma",
     }
     // Add more project objects as needed
 ];
