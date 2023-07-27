14aa0a60c4c7e546cf054837785e4a13087502e7
Aryan0169 <aryanagarwal170@gmail.com>
2023-07-28 01:12:41 +0530

    added n

--- data/file.js
+++ data/file.js
@@ -4,27 +4,26 @@ function getData() {
         {
             mainHeading: 'GS1',
             subHeading: 'History',
-            fileName: 'Hello',
-            fileLink: 'pdf/category1_subA.pdf'
+            fileName: 'Class 6th NCERT',
+            fileLink: 'https://drive.google.com/file/d/1hDeKd0GkHJCUBiCVuEGFUy5oFNymJVav/view?usp=sharing'
         },
         {
             mainHeading: 'GS2',
             subHeading: 'Political Science',
-            fileName: 'Constitution Class 11th',
+            fileName: 'Constitution (Class 11th)',
             fileLink: 'https://drive.google.com/file/d/1alhMkxAdZUwYyCN3DClBl7_uAxxi0xPo/view?usp=sharing'
         },
         {
             mainHeading: 'GS3',
             subHeading: 'Economics',
-            fileName: 'Hello',
-            fileLink: 'pdf/category2_subC.pdf'
+            fileName: 'Indian Economic Development (Class 11th)',
+            fileLink: 'https://drive.google.com/file/d/1h7HgH3I-o3HnSvDiY8cZDIRFYcUIDNhV/view?usp=sharing'
         },
         {
             mainHeading: 'GS3',
-            subHeading: 'Eco',
-            fileName: 'Hello',
+            subHeading: 'Enviroment',
+            fileName: 'Indian Economic Development (Class 11th Last 2 Chapter)',
             fileLink: 'pdf/category2_subD.pdf'
         },
-        // Add more data entries as needed...
     ];
 }
