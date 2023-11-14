e962a0fe094ae82a24bb9b6fe2d762a59306c0f6
Aryan0169 <aryanagarwal170@gmail.com>
2023-11-14 23:46:55 +0530

    ui changes in achievements

--- extrawebpages/data.js
+++ extrawebpages/data.js
@@ -2,15 +2,15 @@ const achievementsData = [
     { name: "Became Treasurer at IEEE SB, LNMIIT", date: "April 2023" },
     { name: "Promoted as Creative Domain Lead", date: "June 2023" },
     { name: "Became Core member of M&M Domai at GDSC", date: "October 2022" },
-    { name: "Joined GDSC's Creative Domain as a member", date: "April 2022" },
-    { name: "Joined GDSC as a Development Domain member", date: " July 2022" },
+    { name: "Joined GDSC Creative Domain as a core member", date: "April 2022" },
+    { name: "Joined GDSC Development Domain as a member", date: " July 2022" },
     { name: "Joined The Robotics Club, The Phoenix", date: "May 2022" },
     { name: "Started my summer internship at LUSIP", date: "May 2023" },
     { name: "Recruited as a UI/UX designer for PLINTH 2023", date: "November 2022" },
     { name: "Recruited as a Public Representative for PLINTH 2023", date: "October 2022" },
-    { name: "Completed my Class 12th with 87.8%", date: "March 2020" },
-    { name: "Completed my Class 10th with 92.5%", date: "April 2018" },
-    // { name: "Received Employee of the Month Award", date: "March 2022" },
+    { name: "Completed my Secondary School with 87.8%", date: "March 2020" },
+    { name: "Completed my Sr. Secondary School with 92.5%", date: "April 2018" },
+    { name: "Advanced my React, HTML,CSS, and JS knowledge and completed my portfolio website.", date: "July 2023" },
     // Add more achievements as needed
   ];
   
