0117da67a1ad10ffc31c1a1fce89c10d55e05ae5
Aryan0169 <aryanagarwal170@gmail.com>
2023-11-25 11:39:08 +0530

    added achievements

--- extrawebpages/data.js
+++ extrawebpages/data.js
@@ -6,13 +6,17 @@ const achievementsData = [
     { name: "Joined GDSC Development Domain as a member", date: " July 2022" },
     { name: "Joined The Robotics Club, The Phoenix", date: "May 2022" },
     { name: "Started my summer internship at LUSIP", date: "May 2023" },
-    { name: "Recruited as a UI/UX designer for PLINTH 2023", date: "November 2022" },
+    { name: "Designer a Website for PLINTH 2023 as a UI/UX designer", date: "December 2022" },
     { name: "Recruited as a Public Representative for PLINTH 2023", date: "October 2022" },
     { name: "Completed my Sr. Secondary School with 87.8%", date: "March 2020" },
     { name: "Completed my Secondary School with 92.5%", date: "April 2018" },
     { name: "Advanced my HTML,CSS, and JS knowledge and completed my portfolio website.", date: "July 2023" },
     { name: "Completed the UI/UX part for the GDSC LNMIIT Website", date: "August 2023" },
     { name: "Organized a Intra-College WebD Hackathon under IEEE SB", date: "September 2023" },
+    { name: "Developed my First Robotics Project, a Gesture Controlled Bot", date: "November 2022" },
+    { name: "Being in Phoenix took a Workshop on Arduino and Basics of Robotics", date: "October 2023" },
+    { name: "Took my second workshop on NodeMCU and how to make a wireless bot via it", date: "April 2023" },
+    { name: "As the event head successfully organized the annual event of Phoenix - The Torque", date: "April 2023" },
   ];
   
   export default achievementsData;
