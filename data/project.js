f81f1b11817cf02f52eb6f882b0847981ce016fb
Aryan0169 <aryanagarwal170@gmail.com>
2023-11-25 11:48:40 +0530

    Changed order of Projects

--- data/project.js
+++ data/project.js
@@ -1,16 +1,16 @@
 const projectsData = [
     {
+        title: 'UAV assisted multiuser DF relayed system',
+        description: 'As a LUSIP summer intern I made use of NOMA technology combined with relay mechanism to improve 6G+ communication',
+        imageUrl: '../images/wireless.jpg',
+        link: 'https://drive.google.com/file/d/14mNKAJsk931b5jZ85hwq7dHyWdEPsR_X/view?usp=drive_link',
+        buttontitle: "Certificate",
+    },
+    {
         title: 'PLINTH 2023',
         description: 'Worked as a UI/UX Developer in making the website for the Annual techfest of LNMIIT, Jaipur.',
         imageUrl: '../images/plinth.png',
         link: 'https://www.plinth.co.in/',
-        buttontitle: "Website",
-    },
-    {
-        title: 'Personal Portfolio',
-        description: 'This is my personal portfolio. I have developed as well as designed this on my own',
-        imageUrl: '../images/pfl.png',
-        link: 'https://www.aryanbansal02.netlify.app/',
         buttontitle: "Website",
     },
     {
@@ -21,18 +21,11 @@ const projectsData = [
         buttontitle: "GITHUB",
     },
     {
-        title: 'Design Projects',
-        description: 'This is my Behance Profile which contain all my design projects',
-        imageUrl: '../images/behance.png',
-        link: 'https://www.behance.net/aryanbansal6',
-        buttontitle: "Behance",
-    },
-    {
-        title: 'UAV assisted multiuser DF relayed system',
-        description: 'As a LUSIP summer intern I made use of NOMA technology combined with relay mechanism to improve 6G+ communication',
-        imageUrl: '../images/wireless.jpg',
-        link: 'https://drive.google.com/file/d/14mNKAJsk931b5jZ85hwq7dHyWdEPsR_X/view?usp=drive_link',
-        buttontitle: "Certificate",
+        title: 'Personal Portfolio',
+        description: 'This is my personal portfolio. I have developed as well as designed this on my own',
+        imageUrl: '../images/pfl.png',
+        link: 'https://www.aryanbansal02.netlify.app/',
+        buttontitle: "Website",
     },
     {
         title: 'GDSC Website',
@@ -40,6 +33,13 @@ const projectsData = [
         imageUrl: '../images/gdsc.png',
         link: 'https://www.figma.com/proto/PFQnOfa19onhSRTomS1Rz7/Website-GDSC?type=design&node-id=612-23658&t=AVq4nIfDq2iAXkXP-0&scaling=scale-down-width&page-id=0%3A1&starting-point-node-id=640%3A2515',
         buttontitle: "Figma",
+    },
+    {
+        title: 'Design Projects',
+        description: 'This is my Behance Profile which contain all my design projects',
+        imageUrl: '../images/behance.png',
+        link: 'https://www.behance.net/aryanbansal6',
+        buttontitle: "Behance",
     }
     // Add more project objects as needed
 ];
